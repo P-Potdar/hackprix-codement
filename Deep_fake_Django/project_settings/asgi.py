82354721665774b183751923bf9a60f0fefa4276
Narendra Singh Rathore <mrperfectth7@gmail.com>
2024-06-08 19:31:20 +0530

    Update: Directory name, django running issue

--- Deep_fake_Django/project_settings/asgi.py
+++ Deep_fake_Django/project_settings/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Deep_fake_Detection project.
+ASGI config for Deep_fake_Django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- Deep_fake_Django/project_settings/urls.py
+++ Deep_fake_Django/project_settings/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Deep_fake_Detection project.
+URL configuration for Deep_fake_Django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
